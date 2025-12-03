6c353b72fbb5762aee5771b6b8ccb8ce8bc991a6
stjmm <shetrynajerkme@gmail.com>
2025-12-03 12:50:13 +0100

    chapter 1

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/user"
+	"monkey/repl"
 )
 
 func main() {
-	fmt.Println("Hello, Go!")
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Welcome %s! This is the Monkey programming lang!\n", user.Username)
+	fmt.Printf("REPL Started\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
