f8e30391f68773f8d28d63e4268b4b913f8a7147
stjmm <shetrynajerkme@gmail.com>
2026-02-06 09:54:17 +0100

    start over

--- README.md
+++
@@ -1,1 +0,0 @@
-Go monkey interpreter.
--- main.go
+++ main.go
@@ -1,18 +1,2 @@
-package main
+package monkey
 
-import (
-	"fmt"
-	"os"
-	"os/user"
-	"monkey/repl"
-)
-
-func main() {
-	user, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("Welcome %s! This is the Monkey programming lang!\n", user.Username)
-	fmt.Printf("REPL Started\n")
-	repl.Start(os.Stdin, os.Stdout)
-}
