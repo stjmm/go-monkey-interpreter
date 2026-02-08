d89930399fefa18ccbd85ca1bb44e91605273234
stjmm <shetrynajerkme@gmail.com>
2026-02-08 11:23:25 +0100

    chapter 1

--- main.go
+++ main.go
@@ -1,2 +1,19 @@
-package monkey
+package main
 
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! You already know what language it is...\n", 
+		user.Username)
+	fmt.Printf("Now type in commands!\n")
+	repl.Start(os.Stdin, os.Stdout)
+}
