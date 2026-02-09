9ac92bb2a1a3b3d62b7679988ea0180d42c309ec
stjmm <shetrynajerkme@gmail.com>
2026-02-09 11:11:46 +0100

    parsing let, return

---
+++ ast/ast.go
@@ -0,0 +1,55 @@
+package ast
+
+import "monkey/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Program - root node
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	} else {
+		return ""
+	}
+}
+
+// LetStatement - `let ...`
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+func (ls *LetStatement) statementNode()       {}
+func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+
+// Identifier `x = ...`
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+
+type ReturnStatement struct {
+	Token token.Token
+	ReturnValue Expression
+}
+func (rs *ReturnStatement) statementNode()      {}
+func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
