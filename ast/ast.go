bafac3eb9eb1489cf6a1f4e83ebde4e977397b1b
stjmm <shetrynajerkme@gmail.com>
2026-02-25 21:08:07 +0100

    infix parsing

--- ast/ast.go
+++ ast/ast.go
@@ -124,10 +124,30 @@ func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
 	var out bytes.Buffer
 
-	out.WriteString("{")
+	out.WriteString("(")
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
-	out.WriteString("}")
+	out.WriteString(")")
+
+	return out.String()
+}
+
+type InfixExpression struct {
+	Token token.Token // The operator as well
+	Left Expression
+	Operator string
+	Right Expression
+}
+func (ie *InfixExpression) expressionNode()      {}
+func (ie *InfixExpression) TokenLiteral() string { return ie.Token.Literal }
+func (ie *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(ie.Left.String())
+	out.WriteString(" " + ie.Operator + " ")
+	out.WriteString(ie.Right.String())
+	out.WriteString(")")
 
 	return out.String()
 }
